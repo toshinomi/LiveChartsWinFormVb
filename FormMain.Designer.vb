75f535224da93721b18f365a0d8adf8de8cbd4fb
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-19 21:50:09 +0900

    プロジェクト ファイルを追加します。

---
+++ FormMain.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnFileSelect = New System.Windows.Forms.Button()
+        Me.image = New System.Windows.Forms.PictureBox()
+        Me.chart = New LiveCharts.WinForms.CartesianChart()
+        CType(Me.image, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnFileSelect
+        '
+        Me.btnFileSelect.BackColor = System.Drawing.Color.White
+        Me.btnFileSelect.Location = New System.Drawing.Point(12, 12)
+        Me.btnFileSelect.Name = "btnFileSelect"
+        Me.btnFileSelect.Size = New System.Drawing.Size(84, 32)
+        Me.btnFileSelect.TabIndex = 0
+        Me.btnFileSelect.Text = "File Select..."
+        Me.btnFileSelect.UseVisualStyleBackColor = False
+        '
+        'image
+        '
+        Me.image.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.image.Location = New System.Drawing.Point(12, 50)
+        Me.image.Name = "image"
+        Me.image.Size = New System.Drawing.Size(325, 371)
+        Me.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.image.TabIndex = 1
+        Me.image.TabStop = False
+        '
+        'chart
+        '
+        Me.chart.Location = New System.Drawing.Point(360, 12)
+        Me.chart.Name = "chart"
+        Me.chart.Size = New System.Drawing.Size(428, 426)
+        Me.chart.TabIndex = 2
+        Me.chart.Text = "CartesianChart1"
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.chart)
+        Me.Controls.Add(Me.image)
+        Me.Controls.Add(Me.btnFileSelect)
+        Me.Name = "FormMain"
+        Me.Text = "Form1"
+        CType(Me.image, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnFileSelect As Button
+    Friend WithEvents image As PictureBox
+    Friend WithEvents chart As LiveCharts.WinForms.CartesianChart
+End Class
---
+++ FormMain.vb
@@ -0,0 +1,102 @@
+﻿Imports LiveCharts
+Imports LiveCharts.Wpf
+Imports System.Drawing.Imaging
+Imports System.Runtime.InteropServices.Marshal
+
+Public Class FormMain
+
+    Private m_nHistgram(256) As Integer
+
+    Public Sub New()
+
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+
+        InitGraph()
+
+    End Sub
+
+    Private Sub BtnFileSelect_Click(sender As Object, e As EventArgs) Handles btnFileSelect.Click
+        Dim openFileDlg As ComOpenFileDialog = New ComOpenFileDialog()
+        openFileDlg.Filter = "JPG|*.jpg|PNG|*.png"
+        openFileDlg.Title = "Open the file"
+        If (openFileDlg.ShowDialog() = True) Then
+            image.Image = Nothing
+            Dim Bitmap = New Bitmap(openFileDlg.FileName)
+
+            image.Image = Bitmap
+
+            DrawHistgram(Bitmap)
+        End If
+    End Sub
+
+    Public Sub InitGraph()
+        Dim lineSeriesChart = New LineSeries With
+        {
+            .Values = New ChartValues(Of Integer),
+            .Title = "Histgram"
+        }
+
+        For nIdx As Integer = 0 To 256
+            lineSeriesChart.Values.Add(0)
+        Next nIdx
+        chart.Series.Clear()
+        chart.Series.Add(lineSeriesChart)
+
+        Return
+    End Sub
+
+    Public Sub DrawHistgram(_bitmap As Bitmap)
+        InitHistgram()
+
+        CalHistgram(_bitmap)
+
+        Dim lineSeriesChart = New LineSeries() With
+        {
+            .Values = New ChartValues(Of Integer),
+            .Title = "Histgram"
+        }
+
+        For nIdx As Integer = 0 To 256
+            lineSeriesChart.Values.Add(m_nHistgram(nIdx))
+        Next nIdx
+        chart.Series.Clear()
+        chart.Series.Add(lineSeriesChart)
+
+        Return
+    End Sub
+
+    Public Sub CalHistgram(_bitmap As Bitmap)
+        Dim nWidthSize As Integer = _bitmap.Width
+        Dim nHeightSize As Integer = _bitmap.Height
+
+        Dim bitmapData As BitmapData = _bitmap.LockBits(New Rectangle(0, 0, nWidthSize, nHeightSize), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb)
+
+        Dim nIdxWidth As Integer
+        Dim nIdxHeight As Integer
+
+        For nIdxHeight = 0 To nHeightSize - 1 Step 1
+            For nIdxWidth = 0 To nWidthSize - 1 Step 1
+
+                Dim pAdr As IntPtr = bitmapData.Scan0
+                Dim nPos As Integer = nIdxHeight * bitmapData.Stride + nIdxWidth * 4
+                Dim nPixelB As Integer = ReadByte(pAdr, nPos + ComInfo.Pixel.B)
+                Dim nPixelG As Integer = ReadByte(pAdr, nPos + ComInfo.Pixel.G)
+                Dim nPixelR As Integer = ReadByte(pAdr, nPos + ComInfo.Pixel.R)
+
+                Dim nGrayScale As Integer = (nPixelB + nPixelG + nPixelR) / 3
+
+                m_nHistgram(nGrayScale) += 1
+            Next
+        Next
+        _bitmap.UnlockBits(bitmapData)
+    End Sub
+
+    Public Sub InitHistgram()
+        For nIdx As Integer = 0 To 256
+            m_nHistgram(nIdx) = 0
+        Next nIdx
+    End Sub
+End Class
